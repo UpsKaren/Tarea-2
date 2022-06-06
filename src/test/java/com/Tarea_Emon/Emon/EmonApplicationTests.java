9fd96a6f683fa006d833f732f3f1839d0901a93d
KAREN <KAREN@DESKTOP-EMQA0KG>
2022-06-06 15:24:27 -0600

    Tarea 2(Mi propia página EmonStore)

---
+++ src/test/java/com/Tarea_Emon/Emon/EmonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tarea_Emon.Emon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
